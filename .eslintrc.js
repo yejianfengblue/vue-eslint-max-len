4d3ab628a7f4b50583bbcf85c2cf9e3b0a128b79
Jian Feng Ye <yejianfengblue@163.com>
2021-02-20 22:04:51 +0800

    Add eslint rule "max-len" and eslint-plugin-vue rule "vue/max-len" does not help

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,8 @@ module.exports = {
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "max-len": ["warn", { code: 120 }],
+    "vue/max-len": ["warn", { code: 120, template: 120 }],
   }
 };
